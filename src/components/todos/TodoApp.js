7176e3af0585571fe17cd2a707deab390a1f58d0
Kyle Bustard <bustardkyle@gmail.com>
2020-02-07 23:42:19 -0600

    Remove get todos feature

--- src/components/todos/TodoApp.js
+++ src/components/todos/TodoApp.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import TodoService from '../../services/TodoService';
+import React from 'react';
 import toastr from '../../toastr';
 import 'toastr/build/toastr.min.css';
 import TodoFooter from './TodoFooter';
@@ -8,30 +7,6 @@ import 'todomvc-common/base.css';
 import 'todomvc-app-css/index.css';
 
 const TodoApp = () => {
-  const [todos, setTodos] = useState([]);
-
-  // initial load of todos data
-  useEffect(() => {
-    try {
-      TodoService.get()
-        .then(response => {
-          setTodos(response.data);
-        })
-        .catch(err => {
-          toastr.error(err);
-        });
-    } catch (error) {
-      toastr.error(error);
-    }
-  }, []);
-  /* The 2nd arg above is a watch list of variables that trigger the effect.
-   * If the list is empty, the effect only executes once (cDM).
-   * If the list is not provided, the effect executes with every change
-   * to props or state (cDM + cDU).
-   * If the list contains some variables, the effect executes whenever any of
-   * those variables are reassigned (cDM, cDU with checks).
-   */
-
   return (
     <section>
       <article className="todoapp">
