143425d2be1223cdabc8ab22e41a9293b383a1e5
Kyle Bustard <bustardkyle@gmail.com>
2020-02-07 21:52:06 -0600

    Update functions

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,75 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import classNames from 'classnames';
 
-const ESCAPE_KEY = 27;
-const ENTER_KEY = 13;
-
 const Todo = ({ todo, toggle, save, remove }) => {
-    const [editing, setEditing] = useState(false);
-    const [editTitle, setEditTitle] = useState(todo.title);
-    const editInputRef = React.createRef();
-    
-    function onEdit() {
-        setEditing(true);
-        editInputRef.current.focus();
-    }
+  const [editing, setEditing] = useState(false);
+  const [editTitle, setEditTitle] = useState(todo.title);
+  const editInputRef = useRef();
 
-    function onSave(event) {
-        const title = editTitle.trim();
-        if (title) {
-            save(todo.id, title);
-            setEditing(false);
-            setEditTitle(title);
-        }
-    }
+  function onEdit() {
+    setEditing(true);
+    editInputRef.current.focus();
+  }
 
-    function onKeyDown(event) {
-        if (event.which === ESCAPE_KEY) {
-            setEditTitle(todo.title);
-            setEditing(false);
-        } else if (event.which === ENTER_KEY) {
-            onSave(event);
-        }
+  function onSave() {
+    const title = editTitle.trim();
+    if (title) {
+      save(todo.id, title);
+      setEditing(false);
+      setEditTitle(title);
     }
+  }
 
-    function onChange(event) {
-        if (editing) {
-            setEditTitle(event.target.value);
-        }
+  function onKeyDown(event) {
+    if (event.key === 'Escape') {
+      setEditTitle(todo.title);
+      setEditing(false);
+    } else if (event.key === 'Enter') {
+      onSave();
     }
+  }
 
-    return (
-        <li className={classNames({
-            completed: todo.completed,
-            editing: editing
-        })}>
-            <div className="view">
-                <input
-                    data-testid={`toggle-button-${todo.id}`}
-                    className="toggle"
-                    type="checkbox"
-                    checked={todo.completed}
-                    onChange={() => {toggle(todo.id)}}
-                />
-                <label onDoubleClick={onEdit}>{editTitle}</label>
-                <button
-                    data-testid={`delete-button-${todo.id}`}
-                    className="destroy"
-                    onClick={() => {remove(todo.id)}}
-                />
-            </div>
-            <input
-                data-testid={`title-input-${todo.id}`}
-                ref={editInputRef}
-                type="text"
-                className="edit"
-                value={editTitle}
-                onBlur={onSave}
-                onChange={onChange}
-                onKeyDown={onKeyDown}
-            />
-        </li>
-    );
+  function onChange(event) {
+    if (editing) {
+      setEditTitle(event.target.value);
+    }
+  }
+
+  return (
+    <li
+      className={classNames({
+        completed: todo.completed,
+        editing: editing
+      })}
+    >
+      <div className="view">
+        <input
+          data-testid={`toggle-button-${todo.id}`}
+          className="toggle"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => {
+            toggle(todo.id);
+          }}
+        />
+        <label onDoubleClick={onEdit}>{editTitle}</label>
+        <button
+          data-testid={`delete-button-${todo.id}`}
+          className="destroy"
+          onClick={() => {
+            remove(todo.id);
+          }}
+        />
+      </div>
+      <input
+        data-testid={`title-input-${todo.id}`}
+        ref={editInputRef}
+        type="text"
+        className="edit"
+        value={editTitle}
+        onBlur={onSave}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+    </li>
+  );
 };
 
 export default Todo;
--- src/components/todos/TodoApp.js
+++ src/components/todos/TodoApp.js
@@ -12,149 +12,160 @@ import 'todomvc-common/base.css';
 import 'todomvc-app-css/index.css';
 
 const TodoApp = () => {
-    const [todos, setTodos] = useState([]);
-    
-    // initial load of todos data
-    useEffect(() => {
-        try {
-            TodoService.get().then(response => {
-                setTodos(response.data)
-            }).catch(err => {
-                toastr.error(err)
-            })
-        } catch(error) {
-            toastr.error(error);
-        }
-    }, []);
-    /* The 2nd arg above is a watch list of variables that trigger the effect.
-     * If the list is empty, the effect only executes once (cDM).
-     * If the list is not provided, the effect executes with every change
-     * to props or state (cDM + cDU).
-     * If the list contains some variables, the effect executes whenever any of
-     * those variables are reassigned (cDM, cDU with checks).
-     */
+  const [todos, setTodos] = useState([]);
 
-    function filterTodos(viewState) {
-        return todos.filter(todo => {
-            switch (viewState) {
-                case ALL_TODOS:
-                    return true;
-                case ACTIVE_TODOS:
-                    return !todo.completed;
-                case COMPLETED_TODOS:
-                    return todo.completed;
-                default:
-                    return false;
-            }
-        });
-    }
-
-    async function onAdd(val) {
-        try {
-            const todo = {
-                title: val,
-                completed: false
-            };
-            const response = await TodoService.post(todo);
-            setTodos([...todos, response.data]);
-        } catch(error) {
-            toastr.error(error);
-        };
-    }
-
-    async function onDelete(id) {
-        try {
-            // Filter all todos except the one to be removed
-            const remaining = todos.filter(todo => todo.id !== id);
-            await TodoService.delete(id);
-            setTodos(remaining);
-        } catch(error) {
-            toastr.error(error);
-        };
-    }
-
-    async function saveTodo(updatedTodo) {
-        try {
-            const response = await TodoService.put(updatedTodo)
-            const updatedTodoFromServer = response.data;
-            const newTodos = todos.map(todo => todo.id !== updatedTodoFromServer.id ? todo : updatedTodoFromServer);
-            setTodos(newTodos);
-        } catch(error) {
-            toastr.error(error);
-        };
-    }
-
-    function onUpdateTitle(id, title) {
-        const foundTodo = todos.find(todo => todo.id === id);
-        const updatedTodo = {
-            ...foundTodo,
-            title,
-        };
-        saveTodo(updatedTodo);
-    }
-
-    function onToggleCompleted(id) {
-        const foundTodo = todos.find(todo => todo.id === id);
-        const updatedTodo = {
-            ...foundTodo,
-            completed: !foundTodo.completed
-        };
-        saveTodo(updatedTodo);
-    }
-
-    function onDeleteCompleted() {
-        const keepers = todos.filter(todo => !todo.completed);
-        const losers = todos.filter(todo => todo.completed);
-        const promises = losers.map( todo => TodoService.delete(todo.id) );
-        Promise.all(promises)
-        .then(responses => {
-            setTodos(keepers)
+  // initial load of todos data
+  useEffect(() => {
+    try {
+      TodoService.get()
+        .then(response => {
+          setTodos(response.data);
         })
-        .catch(error => {
-            toastr.error(error);
+        .catch(err => {
+          toastr.error(err);
         });
+    } catch (error) {
+      toastr.error(error);
     }
+  }, []);
+  /* The 2nd arg above is a watch list of variables that trigger the effect.
+   * If the list is empty, the effect only executes once (cDM).
+   * If the list is not provided, the effect executes with every change
+   * to props or state (cDM + cDU).
+   * If the list contains some variables, the effect executes whenever any of
+   * those variables are reassigned (cDM, cDU with checks).
+   */
 
-    const completedCount = todos.reduce( (acc, todo) => todo.completed ? acc + 1 : acc, 0);
-    const activeCount = todos.length - completedCount;
+  function filterTodos(viewState) {
+    return todos.filter(todo => {
+      switch (viewState) {
+        case ALL_TODOS:
+          return true;
+        case ACTIVE_TODOS:
+          return !todo.completed;
+        case COMPLETED_TODOS:
+          return todo.completed;
+        default:
+          return false;
+      }
+    });
+  }
 
-    return (
-        <section>
-            <article className="todoapp">
-                <header className="header">
-                    <h1 style={{ top: "-175px" }}>todos</h1>
-                    <NewTodoForm addTodo={onAdd} />
-                </header>
+  async function onAdd(val) {
+    try {
+      const todo = {
+        title: val,
+        completed: false
+      };
+      const response = await TodoService.post(todo);
+      setTodos([...todos, response.data]);
+    } catch (error) {
+      toastr.error(error);
+    }
+  }
 
-                <main className="main">
-                        <Route path="/:filter?" render={props => {
-                            const filter = props.match.params.filter || ALL_TODOS;
-                            const todosToShow = filterTodos(filter);
-                        return (
-                            <TodoList
-                                {...props}
-                                todos={todosToShow}
-                                toggle={onToggleCompleted}
-                                remove={onDelete}
-                                save={onUpdateTitle}
-                            />
-                        );
-                    }}/>
-                </main>
-                <TodoFooter
-                    activeCount={activeCount}
-                    completedCount={completedCount}
-                    onClearCompleted={onDeleteCompleted}
+  async function onDelete(id) {
+    try {
+      // Filter all todos except the one to be removed
+      const remaining = todos.filter(todo => todo.id !== id);
+      await TodoService.delete(id);
+      setTodos(remaining);
+    } catch (error) {
+      toastr.error(error);
+    }
+  }
+
+  async function saveTodo(updatedTodo) {
+    try {
+      const response = await TodoService.put(updatedTodo);
+      const updatedTodoFromServer = response.data;
+      const newTodos = todos.map(todo =>
+        todo.id !== updatedTodoFromServer.id ? todo : updatedTodoFromServer
+      );
+      setTodos(newTodos);
+    } catch (error) {
+      toastr.error(error);
+    }
+  }
+
+  function onUpdateTitle(id, title) {
+    const foundTodo = todos.find(todo => todo.id === id);
+    const updatedTodo = {
+      ...foundTodo,
+      title
+    };
+    saveTodo(updatedTodo);
+  }
+
+  function onToggleCompleted(id) {
+    const foundTodo = todos.find(todo => todo.id === id);
+    const updatedTodo = {
+      ...foundTodo,
+      completed: !foundTodo.completed
+    };
+    saveTodo(updatedTodo);
+  }
+
+  function onDeleteCompleted() {
+    const keepers = todos.filter(todo => !todo.completed);
+    const losers = todos.filter(todo => todo.completed);
+    const promises = losers.map(todo => TodoService.delete(todo.id));
+    Promise.all(promises)
+      .then(_responses => {
+        setTodos(keepers);
+      })
+      .catch(error => {
+        toastr.error(error);
+      });
+  }
+
+  const completedCount = todos.reduce(
+    (acc, todo) => (todo.completed ? acc + 1 : acc),
+    0
+  );
+  const activeCount = todos.length - completedCount;
+
+  return (
+    <section>
+      <article className="todoapp">
+        <header className="header">
+          <h1 style={{ top: '-175px' }}>todos</h1>
+          <NewTodoForm addTodo={onAdd} />
+        </header>
+
+        <main className="main">
+          <Route
+            path="/:filter?"
+            render={props => {
+              const filter = props.match.params.filter || ALL_TODOS;
+              const todosToShow = filterTodos(filter);
+              return (
+                <TodoList
+                  {...props}
+                  todos={todosToShow}
+                  toggle={onToggleCompleted}
+                  remove={onDelete}
+                  save={onUpdateTitle}
                 />
-            </article>
+              );
+            }}
+          />
+        </main>
+        <TodoFooter
+          activeCount={activeCount}
+          completedCount={completedCount}
+          onClearCompleted={onDeleteCompleted}
+        />
+      </article>
 
-            <footer className="info">
-                <p>Double-click to edit a todo</p>
-                <p>Created by <a href="http://github.homedepot.com/mah3093/">Dr. Mike Hopper</a></p>
-                <p>For <a href="http://github.homedepot.com/OrangeMethod">OrangeMethod</a></p>
-            </footer>
-        </section>
-    );
-}
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>
+          Created by <a href="https://github.com/drmikeh/">Dr. Mike Hopper</a>
+        </p>
+      </footer>
+    </section>
+  );
+};
 
 export default TodoApp;
